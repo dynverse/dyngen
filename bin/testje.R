5dccfa7ee0c2b26a906515586339a0570d225c3d
Wouter Saelens <wouter.saelens@gmail.com>
2016-04-04 15:15:03 +0200

    smoother transitions between runs

--- bin/testje.R
+++ bin/testje.R
@@ -8,21 +8,31 @@ library(pheatmap)
 
 G = c(1:50)
 tfs = G[1:10]
-target2tfs <- lapply(G, function(g) sample(tfs, 1+rbinom(1, length(tfs)-1, 0.1)))
+target2tfs <- lapply(G, function(g) {
+  if(g%in%tfs) {
+    return(double())
+  } else {
+    return(sample(tfs, 1+rbinom(1, length(tfs)-1, 0.1)))
+  }
+})
 names(target2tfs) = G
 
-# source("ba_network.R")
-# ba.network <- generate.ba(amnt.nodes = 10, amnt.edges = 20)
+## generating reaction formulas (the probability that a reaction occurs in [t, t dt])
 
 interactionid = 0
 kterms = c()
 production = mapply(function(g, tfs) {
-  inputs = sapply(tfs, function(tf) paste0("(x", tf, "/k", tf, "g", g, ")"))
-  
-  kterms <<- c(kterms, sapply(tfs, function(tf) paste0("k", tf, "g", g)))
-  
-  up = paste0("(a0g", g, "+", paste("a1", inputs, sep="*", collapse="+"), ")")
-  down = paste0("(1+", paste0(inputs, collapse="+"), ")")
+  if (length(tfs) > 0) {
+    inputs = sapply(tfs, function(tf) paste0("(x", tf, "/k", tf, "g", g, ")"))
+    
+    kterms <<- c(kterms, sapply(tfs, function(tf) paste0("k", tf, "g", g)))
+    
+    up = paste0("(a0g", g, "+", paste("a1", inputs, sep="*", collapse="+"), ")")
+    down = paste0("(1+", paste0(inputs, collapse="+"), ")")
+  } else {
+    up = paste0("(a0g", g, ")")
+    down = "1"
+  }
   
   #up = paste0(sapply(tfs, function(tf) paste0("(X", tf, "/k", tf, "g", g, ")")), collapse="+")
   return(paste0("r", g, "*", up, "/", down))
@@ -34,23 +44,26 @@ decay = mapply(function(g, tfs) {
 
 formulas = c(production, decay)
 
+## generating the (initial) parameters of the system
+
 R = rlnorm(length(G), log(10))
 names(R) = paste0("r", G)
 D = rep(1,length(G))
 names(D) = paste0("d", G)
-K = sapply(kterms, function(kterm) R[paste0("r",str_replace("k1g2", "k(\\d*)g\\d*", "\\1"))])
+K = sapply(kterms, function(kterm) R[paste0("r",str_replace(kterm, "k(\\d*)g\\d*", "\\1"))]/2)
 names(K) = kterms
 A0 = rep(0.1, length(G))
 names(A0) = paste0("a0g", G)
-A0[sample(tfs, 1+rbinom(1, length(tfs)-1, 0.4))] = 1
 
-params = c(a0=0.1, a1=1, R, D, K, A0)
 nu <- cbind2(diag(nrow=length(G), ncol=length(G)), -diag(nrow=length(G), ncol=length(G)))
-X0 = round(R/4, 0)
+X0 = round(R/2, 0)
 names(X0) = paste0("x", G)
 
-tf = 10
+## simulation
 
+tf = 5
+
+# function to postprocess the ssa output
 process_ssa = function(out, starttime=0) {
   tf = out$args$tf
   rownames(out$data) = NULL
@@ -67,20 +80,47 @@ process_ssa = function(out, starttime=0) {
   return(list(times=times, expression=data))
 }
 
-out <- ssa(X0,formulas,nu,params,tf=tf, method="OTL")
+# we need to perturb the system during differentiation, but ssa does not allow this
+# we therefore combine different ssa runs and each time perturb parameters and set X0 to Xn of the previous run
+
+# first run
+A0[] = 0.05
+A0[sample(tfs, 1+rbinom(1, length(tfs)-1, 0.5))] = 1
+params = c(a1=1, R, D, K, A0)
+
+print(A0)
+
+runtimes = c()
+
+out <- ssa(X0,formulas,nu,params,tf=tf)
 output = process_ssa(out)
 expression = output$expression
 times = output$times
+runtimes[length(runtimes)+1] = last(times)
+runs = rep(1, length(output$times))
 
-X0 = tail(output$expression, n=1)[1,]
-A0[sample(tfs, 1+rbinom(1, length(tfs)-1, 0.4))] = 1
-params = c(a0=0.1, a1=1, R, D, K, A0)
-out <- ssa(X0,formulas,nu,params,tf=tf, method="OTL")
-output = process_ssa(out, last(times))
-expression=rbind2(expression, output$expression)
-times = c(times, output$times)
+# other runs: perturb TF activity
+tf=1
+lapply(c(1:20), function(i) {
+  X0 = tail(expression, n=1)[1,]
+  A0[] = 0.05
+  
+  A0[sample(tfs, 1)] <<- 1
+  A0[sample(tfs, 1)] <<- 0.05
+  #A0[sample(tfs, 1+rbinom(1, length(tfs)-1, 0.5))] = 1
+  print(A0)
+  params = c(a1=1, R, D, K, A0)
+  out <- ssa(X0,formulas,nu,params,tf=tf)
+  output = process_ssa(out, last(times))
+  expression<<-rbind2(expression, output$expression)
+  times <<- c(times, output$times)
+  runtimes[length(runtimes)+1] <<- last(times)
+  runs <<- c(runs, rep(i+1, length(output$times)))
+  return("yay!")
+})
 
-pheatmap(t(expression), scale="row", cluster_cols=F, cluster_rows=T, clustering_distance_rows = "correlation")
+rownames(expression) = c(1:nrow(expression))
+pheatmap(t(expression), scale="row", cluster_cols=F, cluster_rows=T, clustering_distance_rows = "correlation", annotation_col = data.frame(run=factor(runs), row.names = rownames(expression)))
 
 library(ggplot2)
 datadf = melt(expression)
@@ -104,4 +144,4 @@ draw.trajectory.plot(space, sampletimes, trajectory$final.path)
 rownames(E) = c(1:nrow(E))
 draw.trajectory.heatmap(E, trajectory$time, as.factor(cut(sampletimes, breaks=99, labels=F)))
 
-draw.trajectory.heatmap(E, sampletimes, as.factor(cut(sampletimes, breaks=99, labels=F)))
+draw.trajectory.heatmap(E, sampletimes, as.factor(cut(sampletimes, breaks=99, labels=F)), show.labels.row = T)
