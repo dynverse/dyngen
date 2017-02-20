3a47fea1900c1fa1817204b71d6b31ddd3db4672
Wouter Saelens <wouter.saelens@gmail.com>
2017-02-20 16:41:16 +0100

    tebseil

--- scripts/4/4_simulate_functions.R
+++ scripts/4/4_simulate_functions.R
@@ -68,41 +68,12 @@ expression_multiple_cells_split_local = function(burntime, totaltime) {
   ExpressionSet(t(expression), AnnotatedDataFrame(data.frame(time=celltimes, row.names = 1:length(celltimes))))
 }
 
-
-
-
-
-
-counts = Emrna %>% exprs %>% {.*100} %>% round() %>% abs()
 amplify = function(a=0, steps=100, rate=0.05) {
   for (i in 1:steps) {
     a = a + rbinom(length(a), a, rate)
   }
   a
 }
-# result = lapply(c(1, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75), function(n) {
-#   map_dbl(rep(n, 1000), amplify) %>% tibble(a = ., n=n)
-# }) %>% bind_rows()
-# ggplot(result) + geom_density(aes(a, color=n, group=n))
-libprep = function(curcounts, lysisrate = 0.6, capturerate = 0.1, amplify = T, amplifysteps = 100, amplifyrate = c(0.0001, 0.03), sequencerate=0.1) {
-  curcounts = counts_lysed = apply(curcounts, 2, function(row) rmultinom(1, sum(row)*lysisrate, row)[,1])
-  curcounts = counts_captured = apply(curcounts, 2, function(row) rmultinom(1, sum(row)*capturerate, row)[,1])
-  
-  # different rates per cell and per gene
-  amplifyrates_cells = runif(ncol(curcounts), min = amplifyrate[1], max=amplifyrate[2])
-  amplifyrates_genes = runif(nrow(curcounts), min = amplifyrate[1], max=amplifyrate[2])
-  amplifyrates = sqrt(amplifyrates_cells %o% amplifyrates_genes) %>% t
-  
-  if(amplify) {
-    curcounts = counts_amplified = lapply(1:ncol(curcounts), function(colid) {
-      amplify(curcounts[, colid], amplifysteps, amplifyrates[,colid])
-    }) %>% do.call(cbind, .)
-  }
-  
-  curcounts = counts_sequences = apply(curcounts, 2, function(row) rmultinom(1, sum(row)*sequencerate, row+0.000001)[,1]) # pseudocounts added because sum of probs cannot be zero
-  curcounts
-}
-
 
 libprep = function(counts, lysisrate = 0.6, capturerate = 0.1, amplify = T, amplifysteps = 100, amplifyrate = c(0.0001, 0.03), sequencerate=0.1, verbose=F, verbose_plot_cell=50, verbose_follow_gene="x_1") {
   curcounts = counts
