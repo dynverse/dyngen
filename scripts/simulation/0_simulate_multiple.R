3b2c1c6a41ec2cc7f0e5c2bdfbe05af14cd28d5b
Robrecht Cannoodt <brechtus@gmail.com>
2017-03-14 12:42:12 +0100

    Upgrade to Hadley's style guide

--- scripts/simulation/0_simulate_multiple.R
+++ scripts/simulation/0_simulate_multiple.R
@@ -2,8 +2,8 @@ library(dplyr)
 
 ## Run experiments
 # Experiment settings
-nreplicates = 2
-experimentsettings = list(
+nreplicates <- 2
+experimentsettings <- list(
   tibble(modulenetname = "linear", totaltime=4, replicate=seq_len(nreplicates)),
   tibble(modulenetname = "cycle", totaltime=20, replicate=seq_len(nreplicates)),
   tibble(modulenetname = "consecutive_bifurcating", totaltime=6, replicate=seq_len(nreplicates)),
@@ -11,40 +11,40 @@ experimentsettings = list(
   tibble(modulenetname = "trifurcating", totaltime=8, replicate=seq_len(nreplicates))
 ) %>% bind_rows() %>% mutate(ncells=500, experimentname=paste0(modulenetname, "_", replicate))
 
-run_settingid = function(experimentid) {
-  setting = experimentsettings[experimentid,] %>% as.list()
+run_settingid <- function(experimentid) {
+  setting <- experimentsettings[experimentid,] %>% as.list()
   print(setting)
   do.call(wrapper, setting)
 }
 
-wrapper = function(modulenetname, totaltime, replicate, ncells, experimentname) {
+wrapper <- function(modulenetname, totaltime, replicate, ncells, experimentname) {
   dyngen::run_experiment(modulenetname, totaltime, ncells=ncells)
 }
 
-experiments = mclapply(seq_len(nrow(experimentsettings)), run_settingid, mc.cores = 8)
+experiments <- parallel::mclapply(seq_len(nrow(experimentsettings)), run_settingid, mc.cores = 8)
 
 ## Extract gold standard
-gss = mclapply(experiments, function(experiment) {
-  gs = extract_goldstandard(experiment, verbose=T)
+gss <- mclapply(experiments, function(experiment) {
+  gs <- extract_goldstandard(experiment, verbose=T)
   gs
 }, mc.cores=4)
 
-experiments = map2(experiments, gss, function(experiment, gs) {
-  experiment$gs = gs
+experiments <- map2(experiments, gss, function(experiment, gs) {
+  experiment$gs <- gs
   experiment
 })
 
-experiment = experiments[[5]]
+experiment <- experiments[[5]]
 
 ## Run scRNAseq
-platforms = read_tsv("data/platforms.tsv")
-# platforms2 = expand.grid(sequencerate = c(seq_len(10)/10), cellcapturerate = seq_len(10)/10) %>% as.data.frame()
-# platforms = platforms[1, ] %>% select(-sequencerate, -cellcapturerate) %>% data.frame(platforms2)
-# platforms$platformname = seq_len(nrow(platforms)) %>% as.character
-datasets = lapply(experiments, function(experiment) {
+platforms <- read_tsv("data/platforms.tsv")
+# platforms2 <- expand.grid(sequencerate = c(seq_len(10)/10), cellcapturerate = seq_len(10)/10) %>% as.data.frame()
+# platforms <- platforms[1, ] %>% select(-sequencerate, -cellcapturerate) %>% data.frame(platforms2)
+# platforms$platformname <- seq_len(nrow(platforms)) %>% as.character
+datasets <- lapply(experiments, function(experiment) {
   lapply(seq_len(nrow(platforms)), function(platformid) {
-    platform = platforms[platformid, ] %>% as.list
-    dataset = run_scrnaseq(experiment, platform)
+    platform <- platforms[platformid, ] %>% as.list
+    dataset <- run_scrnaseq(experiment, platform)
   })
 }) %>% unlist(recursive=F)
 
@@ -52,7 +52,7 @@ datasets = lapply(experiments, function(experiment) {
 
 saveRDS(datasets, file="results/datasets.rds")
 
-datasets = readRDS("results/datasets.rds")
+datasets <- readRDS("results/datasets.rds")
 
 ##
 
