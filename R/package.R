dc874ebcefc52fc411e0c60b6aaaa3c9704337c8
Robrecht Cannoodt <brechtus@gmail.com>
2017-08-29 09:11:12 +0200

    Initial package documentation added

---
+++ R/package.R
@@ -0,0 +1,11 @@
+#' Generating TI data
+#'
+#' @import dplyr
+#' @import methods
+#' @import tibble
+#' @importFrom purrr %>% map map_df map_chr map_lgl map_int map_dbl keep
+#' @importFrom magrittr %<>%
+#'
+#' @docType package
+#' @name dyngen
+NULL
