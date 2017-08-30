3a2f5f396116b1307aba7add2fa1e29712879858
Wouter Saelens <wouter.saelens@gmail.com>
2017-08-30 13:50:16 +0200

    typo

--- R/toy-1-networks.R
+++ R/toy-1-networks.R
@@ -1,8 +1,10 @@
 #' @export
 generate_toy_milestone_network <- function(ti_type=c("linear", "bifurcating", "cycle")) {
+  if(!(ti_type %in% c("linear", "bifurcating", "cycle"))) stop("invalid ti_type")
+  
   if(identical(ti_type, "linear")) {
     tibble(from="M1", to="M2", length=1)
-  } else if (identical(ti_type, "bifurcatin")) {
+  } else if (identical(ti_type, "bifurcating")) {
     tibble(from=c("M1", "M2", "M2"), to=c("M2", "M3", "M4"), length=1)
   } else if (identical(ti_type, "cycle")) {
     tibble(from=c("M1", "M2", "M3"), to=c("M2", "M3", "M1"), length=1)
