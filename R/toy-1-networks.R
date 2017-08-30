bb22bdd71b1d261616c8ef490ab3ad05a84e6f9f
Wouter Saelens <wouter.saelens@gmail.com>
2017-08-30 13:01:26 +0200

    added toy datasets

---
+++ R/toy-1-networks.R
@@ -0,0 +1,10 @@
+#' @export
+generate_toy_milestone_network <- function(ti_type=c("linear", "bifurcating", "cycle")) {
+  if(identical(ti_type, "linear")) {
+    tibble(from="M1", to="M2", length=1)
+  } else if (identical(ti_type, "bifurcatin")) {
+    tibble(from=c("M1", "M2", "M2"), to=c("M2", "M3", "M4"), length=1)
+  } else if (identical(ti_type, "cycle")) {
+    tibble(from=c("M1", "M2", "M3"), to=c("M2", "M3", "M1"), length=1)
+  }
+}
