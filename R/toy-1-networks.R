faf3298b29bc4606610561c715bde790bf1c44e1
Wouter Saelens <wouter.saelens@gmail.com>
2017-09-01 15:42:53 +0200

    toy linear network simplified

--- R/toy-1-networks.R
+++ R/toy-1-networks.R
@@ -3,7 +3,7 @@ generate_toy_milestone_network <- function(ti_type=c("linear", "bifurcating", "c
   if(!(ti_type %in% c("linear", "bifurcating", "cycle"))) stop("invalid ti_type")
   
   if(identical(ti_type, "linear")) {
-    tibble(from=c("M1", "M2"), to=c("M2", "M3"), length=1)
+    tibble(from=c("M1"), to=c("M2"), length=1)
   } else if (identical(ti_type, "bifurcating")) {
     tibble(from=c("M1", "M2", "M2", "M3", "M4"), to=c("M2", "M3", "M4", "M5", "M6"), length=1)
   } else if (identical(ti_type, "cycle")) {
