cb3857e441bf19d3997a0bcdebb4a02570094c79
Wouter Saelens <wouter.saelens@gmail.com>
2017-10-26 14:23:04 +0200

    parameter inherticance for experiment

--- R/zzz_param_inheritance.R
+++ R/zzz_param_inheritance.R
@@ -1,1 +1,2 @@
 generate_model_from_modulenet <- dynutils::inherit_default_params(c(add_targets_realnet, modulenet_to_genenet, generate_random_tree), generate_model_from_modulenet)
+run_experiment <- dynutils::inherit_default_params(c(take_experiment_cells, filter_expression, add_housekeeping_poisson), run_experiment)
