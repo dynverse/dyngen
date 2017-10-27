06bb935713fecfd49e304a777a0699519aaa40ef
Wouter Saelens <wouter.saelens@gmail.com>
2017-10-27 10:00:05 +0200

    add glue dependency

--- R/package.R
+++ R/package.R
@@ -6,6 +6,7 @@
 #' @import dynutils
 #' @import readr
 #' @import tidyr
+#' @import glue
 #' @importFrom reshape2 melt acast
 #' @importFrom viridis scale_color_viridis
 #' @importFrom purrr %>% map map_df map_chr map_lgl map_int map_dbl keep invoke pmap map2 set_names
