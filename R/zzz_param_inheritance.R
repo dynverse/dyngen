b8035b9fe624bb844481bb11c39bab42a511647b
Wouter Saelens <wouter.saelens@gmail.com>
2017-10-31 09:06:24 +0100

    inherit

--- R/zzz_param_inheritance.R
+++ R/zzz_param_inheritance.R
@@ -1,2 +1,4 @@
-generate_model_from_modulenet <- dynutils::inherit_default_params(c(add_targets_realnet, modulenet_to_genenet, generate_random_tree), generate_model_from_modulenet)
+generate_model_from_modulenet <- dynutils::inherit_default_params(c(add_targets_realnet, modulenet_to_genenet, generate_random_tree, randomize_gene_kinetics), generate_model_from_modulenet)
+generate_system <- dynutils::inherit_default_params(c(randomize_gene_kinetics), generate_system)
+
 run_experiment <- dynutils::inherit_default_params(c(take_experiment_cells, filter_expression, add_housekeeping_poisson), run_experiment)
