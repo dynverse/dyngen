969945e229495ff94f43758c58e1f96f28445931
Wouter Saelens <wouter.saelens@gmail.com>
2017-12-05 10:30:30 +0100

    update to estimate platform and simulation

--- scripts/estimate_platform.R
+++ scripts/estimate_platform.R
@@ -4,32 +4,30 @@ library(tidyverse)
 
 mutate <- dplyr::mutate
 filter <- dplyr::filter
+slice <- dplyr::slice
 
-dataset_id <- "psc_astrocyte_maturation_glia_sloan"
-dataset <- readRDS(glue::glue("../dynalysis/analysis/data/datasets/real/{dataset_id}.rds"))
-dataset$counts %>% dim
-
-counts <- dataset$counts
+# dataset_id <- "psc-astrocyte-maturation-neuron_sloan"
+dataset_id <- "hematopoiesis-clusters_olsson"
+counts <- readRDS(glue::glue("../dynalysis/analysis/data/derived_data/datasets/real/{dataset_id}/original_counts.rds"))
+dataset <- readRDS(glue::glue("../dynalysis/analysis/data/derived_data/datasets/real/{dataset_id}/dataset.rds"))
+counts <- counts[dataset$cell_grouping$cell_id,]
+counts %>% dim
 
 # calculate changing genes
 seurat <- Seurat::CreateSeuratObject(t(counts))
 seurat@ident <- dataset$cell_grouping %>% slice(match(rownames(counts), cell_id)) %>% pull(group_id) %>% factor() %>% setNames(rownames(counts))
 changing <- FindAllMarkers(seurat, logfc.treshold = 1, min.pct=0.4)
-n_changing <- changing %>% filter(abs(avg_logFC) >= 1) %>% rownames() %>% unique() %>% length()
+n_changing <- changing %>% filter(abs(avg_logFC) >= 1) %>% pull(gene) %>% unique() %>% length()
 pct_changing <- n_changing / ncol(counts)
+
+# data("sc_example_counts")
+# counts <- t(sc_example_counts)
 
 # splatter estimate libSizes etc
 # splatter requires non-zero rows and columns to estimate
 cell_sds <- counts %>% apply(1, sd)
 gene_sds <- counts %>% apply(2, sd)
 counts <- counts[cell_sds > 0, gene_sds > 0]
-
-counts <- counts[, sample(colnames(counts), 1000)]
-
-cell_sds <- counts %>% apply(1, sd)
-gene_sds <- counts %>% apply(2, sd)
-counts <- counts[cell_sds > 0, gene_sds > 0]
-
 counts %>% dim
 
 # estimate splatter parameters
