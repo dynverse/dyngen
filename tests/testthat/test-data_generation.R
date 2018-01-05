53a25fe49356eca4b3c72633f401bc8f8b12a7df
Wouter Saelens <wouter.saelens@gmail.com>
2018-01-05 09:58:37 +0100

    avoid creation of an Rplots.pdf

--- tests/testthat/test-data_generation.R
+++ tests/testthat/test-data_generation.R
@@ -5,14 +5,18 @@ test_that("a full dataset can be generated", {
   options(ncores = 1)
   
   model <- invoke(generate_model_from_modulenet, params$model)
+  pdf(tempfile())
   plot_model(model)
+  dev.off()
   
   simulation <- invoke(simulate_multiple, params$simulation, model$system)
   simulation <- preprocess_simulation_for_gs(simulation, model, params$gs$smooth_window)
   plot_simulation(simulation)
   
   gs <- invoke(extract_goldstandard, params$gs, simulation, model, preprocess=FALSE)
+  pdf(tempfile())
   plot_goldstandard(simulation, gs)
+  dev.off()
   
   experiment <- invoke(run_experiment, params$experiment, simulation, gs)
   
