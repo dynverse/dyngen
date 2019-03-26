7f6919cd1fe2eea9387b0c2be846665e555f38e0
Robrecht Cannoodt <rcannood@gmail.com>
2019-03-26 16:50:34 +0100

    move most of realnet data to separate branch

--- data-raw/realnet.R
+++ data-raw/realnet.R
@@ -8,21 +8,28 @@ tmp_dir <- tempfile()
 unzip(tmp_zip, exdir = tmp_dir)
 on.exit(unlink(tmp_dir, recursive = TRUE, force = TRUE))
 
-set.seed(1)
+metadf <- 
+  list.files(
+    paste0(tmp_dir, "/Network_compendium/Tissue-specific_regulatory_networks_FANTOM5-v1/32_high-level_networks"), 
+    full.names = TRUE,
+    pattern = "txt.gz"
+  ) %>% 
+  tibble(file = .) %>% 
+  mutate(
+    name = file %>% str_replace("^.*/", "") %>% str_replace("\\.txt\\.gz$", "") %>% paste0("regulatorycircuits_", .),
+    url = paste0("https://github.com/dynverse/dyngen/raw/data_files/", name, ".rds")
+  )
 
-network_files <- list.files(
-  paste0(tmp_dir, "/Network_compendium/Tissue-specific_regulatory_networks_FANTOM5-v1/32_high-level_networks"), 
-  full.names = TRUE,
-  pattern = "txt.gz"
-) %>% 
-  sample(4) %>% 
-  sort()
+output_dir <- tempfile()
+dir.create(output_dir)
 
-network_names <- network_files %>% str_replace("^.*/", "") %>% str_replace("\\.txt\\.gz$", "")
-
-realnets <- map(
-  network_files,
-  function(file) {
+pbapply::pblapply(
+  seq_len(nrow(metadf)),
+  cl = 8,
+  function(i) {
+    file <- metadf$file[[i]]
+    name <- metadf$name[[i]]
+    
     df <- 
       read_tsv(file, col_names = c("regulator", "target", "weight"), col_types = c("regulator" = "c", "target" = "c", "weight" = "d")) %>% 
       arrange(desc(weight)) %>% 
@@ -30,14 +37,25 @@ realnets <- map(
     
     genes <- unique(c(df$regulator, df$target))
     
-    df %>% 
-      mutate(
-        regulator = match(regulator, genes),
-        target = match(target, genes)
+    m <- Matrix::sparseMatrix(
+      i = match(df$regulator, genes),
+      j = match(df$target, genes),
+      x = df$weight,
+      dimnames = list(
+        genes[sort(unique(match(df$regulator, genes)))],
+        genes
       )
+    )
+    
+    write_rds(m, paste0(output_dir, "/", name, ".rds"), compress = "xz")
   }
-) %>% 
-  set_names(network_names)
+)
 
-pryr::object_size(realnet)
-usethis::use_data(realnet, compress = "xz", overwrite = TRUE)
+cat("Output is stored at ", output_dir, "\n", sep = "")
+
+regulatorycircuits_01_neurons_fetal_brain <- read_rds(paste0(output_dir, "/regulatorycircuits_01_neurons_fetal_brain.rds"))
+
+usethis::use_data(regulatorycircuits_01_neurons_fetal_brain, compress = "xz", overwrite = TRUE)
+
+realnets <- metadf %>% select(name, url)
+usethis::use_data(realnets, compress = "xz", overwrite = TRUE)
