3be5dafc4ad030b20e04eb9f3c4b7c062c2d09f3
Robrecht Cannoodt <rcannood@gmail.com>
2019-03-26 09:38:08 +0100

    add back package object

---
+++ R/package.R
@@ -0,0 +1,19 @@
+#' Generating TI data
+#'
+#' @import dplyr
+#' @import methods
+#' @importFrom tibble is_tibble as_tibble as_data_frame tibble data_frame enframe deframe lst tribble rownames_to_column column_to_rownames
+#' @import dynutils
+#' @import dynwrap
+#' @import readr
+#' @import tidyr
+#' @import ggplot2
+#' @import assertthat
+#' @importFrom reshape2 melt acast
+#' @importFrom viridis scale_color_viridis
+#' @importFrom purrr %>% map map_df map_chr map_lgl map_int map_dbl keep invoke pmap map2 set_names imap
+#' @importFrom magrittr %<>%
+#'
+#' @docType package
+#' @name dyngen
+NULL
