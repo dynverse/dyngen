9eedacc5bc758a9d835718af25fb8d1b9169c982
Robrecht Cannoodt <rcannood@gmail.com>
2019-03-27 19:34:10 +0100

    save example script

--- example_script.R
+++ example_script.R
@@ -3,7 +3,7 @@ devtools::load_all(".")
 model <- 
   generator_config(
     modulenet = modulenet_consecutive_bifurcating(),
-    platform = platform_simple(n_cells = 1000, n_features = 10000),
+    platform = platform_simple(n_cells = 1000, n_features = 10000, pct_trajectory_features = 0.2),
     verbose = TRUE
   ) %>% 
   generate_tfnet() %>% 
