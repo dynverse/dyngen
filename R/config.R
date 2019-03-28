e206a1d63113eca85cacc37221990f0033de91d2
Robrecht Cannoodt <rcannood@gmail.com>
2019-03-28 18:13:22 +0100

    update config

--- R/config.R
+++ R/config.R
@@ -4,23 +4,25 @@ initialise_model <- function(
   platform = platform_simple(),
   tfgen_params = tfgen_random(),
   networkgen_params = networkgen_realnet_sampler(),
-  kinetics_params = kinetics_default(),
+  simulation_setup_params = simulation_setup_default(),
   simulation_params = simulation_default(),
   goldstandard_params = goldstandard_default(),
   experiment_sampler = experiment_sampler_snapshot(),
   normalisation_params = normalisation_default(),
-  verbose = FALSE
+  verbose = FALSE,
+  num_cores = 1
 ) {
   lst(
     modulenet,
     platform,
     tfgen_params,
     networkgen_params,
-    kinetics_params,
+    simulation_setup_params,
     simulation_params,
     goldstandard_params,
     experiment_sampler,
     normalisation_params,
-    verbose
+    verbose,
+    num_cores
   )
 }
