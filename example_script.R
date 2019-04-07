19a6ce4e60b2c0fc1035c0d4810d1e0abdb3d4a2
Robrecht Cannoodt <rcannood@gmail.com>
2019-04-07 15:12:06 +0200

    update scripts

--- example_script.R
+++ example_script.R
@@ -4,126 +4,130 @@ set.seed(1)
 model <- 
   initialise_model(
     modulenet = modulenet_bifurcating_converging(),
-    platform = platform_simple(n_cells = 1000, n_features = 10 * 3, pct_main_features = 1),
+    platform = platform_simple(n_cells = 1000, n_features = 5 * 10, pct_main_features = 1),
     tfgen_params = tfgen_random(percentage_tfs = 1, min_tfs_per_module = 3),
-    simulation_params = simulation_default(total_time = 10, num_simulations = 12),
+    simulation_params = simulation_default(total_time = 10, num_simulations = 32),
     simulation_setup = simulation_setup_custom(),
     verbose = TRUE,
-    num_cores = 6
+    num_cores = 8
   ) %>% 
-  generate_tf_network() #%>% 
-  # generate_feature_network()
+  generate_tf_network() %>% 
+  generate_feature_network() %>% 
+  generate_simulation_setup() %>% 
+  simulate_cells()
 
 
 plot_module_network(model)
 # plot_feature_network(model, tfs_only = TRUE)
 plot_feature_network(model)
-
-model <- 
-  model %>%
-  generate_simulation_setup() %>% 
-  simulate_cells()
-
 plot_simulations(model)
+
+model$goldstandard_params <- goldstandard_default(time_per_edge = 3)
+model <- 
+  model %>% 
+  generate_goldstandard()
+
+
+plot_gold_simulations(model)
 
 # write_rds(model, "~/yay3.rds")
 # write_rds(model, "~/yay2.rds")
 # model <- read_rds("~/yay.rds")
 
-library(gganimate)
-
-# get feature info
-feature_info <- 
-  model$feature_info %>% 
-  mutate(
-    main_index = match(is_main, c(TRUE, FALSE, NA)),
-    size = c(4, 1, 1)[main_index],
-    label = ""
-  ) %>% 
-  filter(is_main)
-
-# get feature network
-feature_network <- 
-  model$feature_network %>% 
-  filter(from %in% feature_info$feature_id & to %in% feature_info$feature_id)
-
-# add extra edges invisible between regulators from the same module
-feature_network <- 
-  bind_rows(
-    feature_network,
-    feature_info %>%
-      filter(is_tf) %>% 
-      select(module_id, feature_id) %>%
-      group_by(module_id) %>%
-      do({
-        crossing(from = .$feature_id, to = .$feature_id) %>%
-          mutate(effect = -2)
-      }) %>% 
-      ungroup() %>% 
-      filter(from < to)
-  )
-
-# determine colors
-feature_network <-
-  feature_network %>% 
-  mutate(
-    color = effect_colour(effect)
-  )
-
-# construct graph
-graph <- igraph::graph_from_data_frame(
-  feature_network,
-  vertices = feature_info
-)
-
-# layout
-#layout <- igraph::layout.fruchterman.reingold(graph) %>% as.data.frame()
-layout <- igraph::layout_with_kk(graph) %>% as.data.frame()
-colnames(layout) <- c("comp1", "comp2")
-rownames(layout) <- feature_info$feature_id
-layout <- layout %>% rownames_to_column("feature_id")
-
-edges <- 
-  feature_network %>% 
-  filter(effect != -2) %>% 
-  left_join(layout %>% rename_all(function(x) paste0("from_", x)), by = c("from" = "from_feature_id")) %>% 
-  left_join(layout %>% rename_all(function(x) paste0("to_", x)), by = c("to" = "to_feature_id"))
-
-plot_df <- 
-  feature_info %>% 
-  left_join(layout, by = "feature_id") %>% 
-  left_join(
-    model$simulations %>% 
-      # filter(simulation_i == 1) %>% 
-      select(t, simulation_i, one_of(feature_info$x)) %>% 
-      gather(x, value, -t, -simulation_i),
-    by = "x"
-  ) %>% 
-  mutate(
-    colour_expr = grDevices::colorRampPalette(rev(RColorBrewer::brewer.pal(n = 7, name = "RdYlBu")))(100)[cut(value, breaks = 100)]
-  )
-
-ggplot(plot_df %>% filter(t == sample(t, 1), simulation_i == 1)) +
-  geom_segment(aes(x = from_comp1, xend = to_comp1, y = from_comp2, yend = to_comp2, linetype = factor(effect, level = c(1, -1))), edges, arrow = arrow(), colour = "darkgray") +
-  geom_point(aes(comp1, comp2, colour = colour_expr), size = 5) +
-  geom_text(aes(comp1, comp2, colour = color, label = feature_id), nudge_y = .2) +
-  scale_colour_identity() +
-  theme_bw() +
-  coord_equal() + 
-  facet_wrap(~simulation_i)
-
-g <-
-  ggplot(plot_df %>% filter(simulation_i == 1)) +
-  geom_segment(aes(x = from_comp1, xend = to_comp1, y = from_comp2, yend = to_comp2, linetype = factor(effect, level = c(1, -1))), edges, arrow = arrow(), colour = "darkgray") +
-  geom_point(aes(comp1, comp2, colour = colour_expr), size = 5) +
-  geom_text(aes(comp1, comp2, colour = color, label = feature_id), nudge_y = .2) +
-  scale_colour_identity() +
-  theme_bw() +
-  coord_equal() + 
-  facet_wrap(~simulation_i) +
-  labs(title = "time = {current_frame}") +
-  transition_manual(t)
-# animate(g, fig.width = 1000, fig.height = 1000)
-options(gganimate.dev_args = list(width = 1000, height = 1000))
-print(g)
-anim_save(filename = "~/help.gif")
+# library(gganimate)
+# 
+# # get feature info
+# feature_info <- 
+#   model$feature_info %>% 
+#   mutate(
+#     main_index = match(is_main, c(TRUE, FALSE, NA)),
+#     size = c(4, 1, 1)[main_index],
+#     label = ""
+#   ) %>% 
+#   filter(is_main)
+# 
+# # get feature network
+# feature_network <- 
+#   model$feature_network %>% 
+#   filter(from %in% feature_info$feature_id & to %in% feature_info$feature_id)
+# 
+# # add extra edges invisible between regulators from the same module
+# feature_network <- 
+#   bind_rows(
+#     feature_network,
+#     feature_info %>%
+#       filter(is_tf) %>% 
+#       select(module_id, feature_id) %>%
+#       group_by(module_id) %>%
+#       do({
+#         crossing(from = .$feature_id, to = .$feature_id) %>%
+#           mutate(effect = -2)
+#       }) %>% 
+#       ungroup() %>% 
+#       filter(from < to)
+#   )
+# 
+# # determine colors
+# feature_network <-
+#   feature_network %>% 
+#   mutate(
+#     color = effect_colour(effect)
+#   )
+# 
+# # construct graph
+# graph <- igraph::graph_from_data_frame(
+#   feature_network,
+#   vertices = feature_info
+# )
+# 
+# # layout
+# #layout <- igraph::layout.fruchterman.reingold(graph) %>% as.data.frame()
+# layout <- igraph::layout_with_kk(graph) %>% as.data.frame()
+# colnames(layout) <- c("comp1", "comp2")
+# rownames(layout) <- feature_info$feature_id
+# layout <- layout %>% rownames_to_column("feature_id")
+# 
+# edges <- 
+#   feature_network %>% 
+#   filter(effect != -2) %>% 
+#   left_join(layout %>% rename_all(function(x) paste0("from_", x)), by = c("from" = "from_feature_id")) %>% 
+#   left_join(layout %>% rename_all(function(x) paste0("to_", x)), by = c("to" = "to_feature_id"))
+# 
+# plot_df <- 
+#   feature_info %>% 
+#   left_join(layout, by = "feature_id") %>% 
+#   left_join(
+#     model$simulations %>% 
+#       # filter(simulation_i == 1) %>% 
+#       select(t, simulation_i, one_of(feature_info$x)) %>% 
+#       gather(x, value, -t, -simulation_i),
+#     by = "x"
+#   ) %>% 
+#   mutate(
+#     colour_expr = grDevices::colorRampPalette(rev(RColorBrewer::brewer.pal(n = 7, name = "RdYlBu")))(100)[cut(value, breaks = 100)]
+#   )
+# 
+# ggplot(plot_df %>% filter(t == sample(t, 1), simulation_i == 1)) +
+#   geom_segment(aes(x = from_comp1, xend = to_comp1, y = from_comp2, yend = to_comp2, linetype = factor(effect, level = c(1, -1))), edges, arrow = arrow(), colour = "darkgray") +
+#   geom_point(aes(comp1, comp2, colour = colour_expr), size = 5) +
+#   geom_text(aes(comp1, comp2, colour = color, label = feature_id), nudge_y = .2) +
+#   scale_colour_identity() +
+#   theme_bw() +
+#   coord_equal() + 
+#   facet_wrap(~simulation_i)
+# 
+# g <-
+#   ggplot(plot_df %>% filter(simulation_i == 1)) +
+#   geom_segment(aes(x = from_comp1, xend = to_comp1, y = from_comp2, yend = to_comp2, linetype = factor(effect, level = c(1, -1))), edges, arrow = arrow(), colour = "darkgray") +
+#   geom_point(aes(comp1, comp2, colour = colour_expr), size = 5) +
+#   geom_text(aes(comp1, comp2, colour = color, label = feature_id), nudge_y = .2) +
+#   scale_colour_identity() +
+#   theme_bw() +
+#   coord_equal() + 
+#   facet_wrap(~simulation_i) +
+#   labs(title = "time = {current_frame}") +
+#   transition_manual(t)
+# # animate(g, fig.width = 1000, fig.height = 1000)
+# options(gganimate.dev_args = list(width = 1000, height = 1000))
+# print(g)
+# anim_save(filename = "~/help.gif")
--- example_test_gs_script_v2.R
+++ example_test_gs_script_v2.R
@@ -1,158 +1,33 @@
 library(tidyverse)
 devtools::load_all(".")
 
-model <- read_rds("~/yay3.rds")
+set.seed(1)
+model <- 
+  initialise_model(
+    modulenet = modulenet_bifurcating_converging(),
+    platform = platform_simple(n_cells = 1000, n_features = 5 * 10, pct_main_features = 1),
+    tfgen_params = tfgen_random(percentage_tfs = 1, min_tfs_per_module = 3),
+    simulation_params = simulation_default(total_time = 10, num_simulations = 32),
+    simulation_setup = simulation_setup_custom(),
+    verbose = TRUE,
+    num_cores = 8
+  ) %>% 
+  generate_tf_network() %>% 
+  generate_feature_network() %>% 
+  generate_simulation_setup() %>%
+  simulate_cells()
 
 plot_module_network(model)
+plot_feature_network(model)
+plot_simulations(model)
 
-sim_params <- model$simulation_params
-sim_system <- model$simulation_system
+model <- model %>% generate_goldstandard()
 
-propensity_funs <-
-  sim_system$formulae %>% 
-  select(formula_id, formula) %>% 
-  deframe
+plot_gold_simulations(model)
 
 
-# find gold transition order
-mod_changes <- 
-  model$modulenet$expression_patterns %>% 
-  mutate(
-    mod_diff = module_progression %>% str_split("\\|"),
-    substate = map(mod_diff, seq_along)
-  ) %>% 
-  select(-module_progression) %>% 
-  unnest(mod_diff, substate) %>% 
-  mutate(
-    mod_diff = str_split(mod_diff, ","),
-    mod_on = map(mod_diff, function(x) x %>% keep(grepl("\\+", .)) %>% gsub("\\+", "", .)),
-    mod_off = map(mod_diff, function(x) x %>% keep(grepl("-", .)) %>% gsub("-", "", .))
-  ) %>% 
-  group_by(from, to) %>% 
-  mutate(
-    from_ = ifelse(row_number() == 1, from, c("", paste0(from, to, letters[row_number() - 1]))),
-    to_ = ifelse(row_number() == n(), to, from_[row_number() + 1])
-  ) %>% 
-  ungroup()
-
-gr <- igraph::graph_from_data_frame(mod_changes %>% select(from_, to_))
-
-mod_order <- igraph::topo_sort(gr) %>% names()
 
 
-# start constructing golds
-gold_sim_outputs <- list()
-gold_sim_vectors <- list()
-gold_sim_modson <- list()
-
-gold_sim_vectors[[mod_order[[1]]]] <- sim_system$initial_state %>% as.matrix
-gold_sim_modson[[mod_order[[1]]]] <- c()
-
-for (i in seq(2, length(mod_order))) {
-  to_ <- mod_order[[i]]
-  
-  for (j in which(mod_changes$to_ == to_)) {
-    from_ <- mod_changes$from_[[j]]
-    mod_on <- mod_changes$mod_on[[j]]
-    mod_off <- mod_changes$mod_off[[j]]
-    prev_mods <- gold_sim_modson[[from_]]
-    mods <- union(mod_on, prev_mods) %>% setdiff(mod_off)
-    
-    cat("Processing ", to_, " := ", from_, " ~ ", paste(mods, collapse = "|"), "\n", sep = "")
-    
-    # fetch initial state
-    new_initial_state <- rowMeans(gold_sim_vectors[[from_]])
-
-    # generate nus
-    new_nus <- sim_system$nus
-    filt <- colnames(new_nus) %>% purrr:::discard(grepl(paste0("_(", paste(mods, collapse = "|"), ")_"), .))
-    new_nus[, filt] <- new_nus[, filt] * 0
-
-    # actual simulation
-    out <- fastgssa::ssa(
-      initial.state = new_initial_state,
-      propensity.funs = propensity_funs,
-      nu = new_nus %>% as.matrix,
-      final.time = sim_params$burn_time,
-      parms = sim_system$parameters,
-      method = sim_params$ssa_algorithm,
-      recalculate.all = TRUE,
-      stop.on.negstate = FALSE,
-      stop.on.propensity = FALSE,
-      verbose = FALSE
-    )
-
-    # add both burnin as normal simulation together
-    simulation <-
-      .simulate_cells_process_ssa(out) %>%
-      mutate(
-        j,
-        from_,
-        to_
-      ) %>%
-      select(j, from_, to_, everything())
-
-    end_state <-
-      simulation %>%
-      slice(n()) %>%
-      select(one_of(sim_system$molecule_ids)) %>%
-      .[1, , drop = TRUE] %>%
-      unlist() %>%
-      as.matrix()
-
-    gold_sim_outputs[[j]] <- simulation
-    
-    gold_sim_modson[[to_]] <- mods
-    
-    if (!to_ %in% gold_sim_vectors) {
-      gold_sim_vectors[[to_]] <- end_state
-    } else {
-      gold_sim_vectors[[to_]] <- cbind(gold_sim_vectors[[to_]], end_state)
-    }
-    
-  }
-}
-  
-
-########################################
-###             combine              ###
-########################################
-
-simulation <- 
-  bind_rows(gold_sim_outputs) %>% 
-  left_join(mod_changes %>% select(from, to, from_, to_, substate), by = c("from_", "to_")) %>% 
-  group_by(from, to) %>% 
-  mutate(
-    simulation_i = 0,
-    type = paste0(from, "->", to),
-    time = ((substate - 1) * 2 + t) / 2 / max(substate)
-  ) %>% 
-  ungroup()
-
-model_new <- model
-model_new$simulations <- bind_rows(simulation, model$simulations %>% mutate(type = paste0("simulation")))
-
-########################################
-###              plot                ###
-########################################
-# expr <- model_new$simulations %>% select(one_of(model_new$simulation_system$molecule_ids)) %>% as.matrix
-sim_f <- model_new$simulations %>% select(t, simulation_i, type, from_, to_, j, from, to, time, substate)
-expr <- model_new$simulations %>% select(-one_of(colnames(sim_f))) %>% as.matrix
-
-space <- SCORPIUS::reduce_dimensionality(expr, dist_fun = SCORPIUS::correlation_distance, num_landmarks = 2000, ndim = 10)
-plot_df <- bind_cols(sim_f, as.data.frame(space))
-
-g1 <- ggplot(mapping = aes(Comp1, Comp4, colour = type, group = paste0(type, "_", simulation_i))) +
-  geom_path(data = plot_df %>% filter(simulation_i > 0), colour = "darkgray") +
-  geom_path(data = plot_df %>% filter(simulation_i == 0), size = 2) +
-  theme_bw()
-
-g2 <- ggplot(mapping = aes(Comp1, Comp4, colour = paste0(from_, "->", to_), group = paste0(type, "_", simulation_i))) +
-  geom_path(data = plot_df %>% filter(simulation_i > 0), colour = "darkgray") +
-  geom_path(data = plot_df %>% filter(simulation_i == 0), size = 2) +
-  theme_bw()
-
-patchwork::wrap_plots(g1, g2)
 
 
 
