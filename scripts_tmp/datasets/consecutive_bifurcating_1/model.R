d82e74026353771fd66d1be918b3186cbdb78d91
Robrecht Cannoodt <rcannood@gmail.com>
2019-04-16 10:11:08 +0200

    move scripts

---
+++ scripts_tmp/datasets/consecutive_bifurcating_1/model.R
@@ -0,0 +1,19 @@
+set.seed(1)
+model <- 
+  initialise_model(
+    num_cells = 1000,
+    num_tfs = 60,
+    num_targets = 300,
+    num_hks = 500,
+    distance_metric = "pearson",
+    backbone = backbone_bifurcating_converging(),
+    tf_network_params = tf_network_random(min_tfs_per_module = 3),
+    feature_network_params = feature_network_default(),
+    kinetics_params = kinetics_custom(sample_strength = function(n) runif(n, 10, 20)),
+    gold_standard_params = gold_standard_default(),
+    simulation_params = simulation_default(total_time = 10, num_simulations = 32),
+    experiment_params = experiment_snapshot(),
+    verbose = TRUE,
+    num_cores = 8,
+    download_cache_dir = "~/.cache/dyngen"
+  )
