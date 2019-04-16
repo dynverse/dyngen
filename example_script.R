7e278c835a0150eb134cad98460ea92ec888ec79
Robrecht Cannoodt <rcannood@gmail.com>
2019-04-16 10:09:59 +0200

    rename files and functions

--- example_script.R
+++ example_script.R
@@ -1,20 +1,20 @@
 library(tidyverse)
 library(dyngen)
 
-set.seed(3)
+set.seed(11)
 model <- 
   initialise_model(
     num_cells = 1000,
-    num_tfs = 50,
-    num_targets = 200,
-    num_hks = 300,
+    num_tfs = 60,
+    num_targets = 300,
+    num_hks = 500,
     distance_metric = "pearson",
     backbone = backbone_bifurcating_converging(),
     tf_network_params = tf_network_random(min_tfs_per_module = 3),
     feature_network_params = feature_network_default(),
-    kinetics_params = kinetics_default(),
-    simulation_params = simulation_default(total_time = 10, num_simulations = 32),
+    kinetics_params = kinetics_custom(sample_strength = function(n) runif(n, 10, 20)),
     gold_standard_params = gold_standard_default(),
+    simulation_params = simulation_default(total_time = 10, num_simulations = 32),
     experiment_params = experiment_snapshot(),
     verbose = TRUE,
     num_cores = 8,
@@ -26,24 +26,20 @@ model <-
   generate_gold_standard() %>% 
   generate_cells() %>% 
   generate_experiment()
-
+  
 plot_backbone(model)
+plot_feature_network(model, tfs_only = TRUE)
 plot_feature_network(model)
-plot_simulations(model)
+plot_gold_simulations(model) + scale_colour_brewer(palette = "Dark2")
 plot_gold_simulations(model) + scale_colour_brewer(palette = "Dark2")
 plot_gold_mappings(model) + scale_colour_brewer(palette = "Dark2")
+plot_simulations(model)
 
+traj <- 
+  model %>% 
+  wrap_dyngen_dataset()
 
-g <- 
-  ggplot(model$simulations$meta %>% filter(t >= 0, simulation_i <= 16) %>% mutate(edge = paste0(from, "_", to))) + 
-  geom_path(aes(t, simulation_i + time, colour = edge, group = simulation_i), size = 1) +
-  theme_bw() +
-  scale_colour_brewer(palette = "Dark2") +
-  scale_y_continuous(breaks = seq(1, 17))
-
-ggsave(plot = g, filename = "~/gs.pdf", width = 20, height = 16)
-
-traj <- wrap_dyngen_dataset(model)
-dynplot::plot_dimred(traj)
-dynplot::plot_default(traj)
+g1 <- dynplot::plot_dimred(traj)
+g2 <- dynplot::plot_default(traj)
+patchwork::wrap_plots(g1, g2, nrow = 1)
 dynplot::plot_heatmap(traj, features_oi = 100)
