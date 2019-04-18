addb0b5a72f939e1a9efd419333a6a809ef6524f
Robrecht Cannoodt <rcannood@gmail.com>
2019-04-18 13:12:25 +0200

    update plots in complete function

--- R/complete_function.R
+++ R/complete_function.R
@@ -6,7 +6,8 @@ complete_function <- function(model, directory) {
   ## GENERATE FEATURE NETWORK
   model <- model %>% 
     generate_tf_network() %>% 
-    generate_feature_network()
+    generate_feature_network() %>% 
+    generate_kinetics()
   
   g1 <- plot_backbone(model)
   g2 <- plot_feature_network(model, tfs_only = TRUE)
@@ -19,22 +20,25 @@ complete_function <- function(model, directory) {
   
   ## GENERATE GOLD STANDARD
   model <- model %>% 
-    generate_kinetics() %>% 
     generate_gold_standard()
   
-  g4 <- plot_gold_simulations(model) + scale_colour_brewer(palette = "Dark2")
+  g4 <- plot_gold_simulations(model) + scale_colour_brewer(palette = "Set3")
+  g5 <- plot_gold_expression(model)
   ggsave(paste0(directory, "/plot_2_gold.pdf"), g4, width = 8, height = 8)
+  ggsave(paste0(directory, "/plot_3_gold_expression.pdf"), g5, width = 12, height = 12)
   
   ## GENERATE CELLS
   model <- model %>%  
     generate_cells() 
   
-  g5 <- plot_gold_simulations(model) + scale_colour_brewer(palette = "Dark2")
-  g6 <- plot_gold_mappings(model) + scale_colour_brewer(palette = "Dark2")
-  g7 <- plot_simulations(model)
-  g <- patchwork::wrap_plots(g5, g7, nrow = 1)
-  ggsave(paste0(directory, "/plot_3_simulations.pdf"), g, width = 12, height = 6)
-  ggsave(paste0(directory, "/plot_4_gold_mappings.pdf"), g6, width = 12, height = 10)
+  g6 <- plot_gold_simulations(model) + scale_colour_brewer(palette = "Set3")
+  g7 <- plot_gold_mappings(model) + scale_colour_brewer(palette = "Set3")
+  g8 <- plot_simulations(model)
+  g9 <- plot_simulation_expression(model)
+  g <- patchwork::wrap_plots(g6, g8, nrow = 1)
+  ggsave(paste0(directory, "/plot_4_simulations.pdf"), g, width = 12, height = 6)
+  ggsave(paste0(directory, "/plot_5_gold_mappings.pdf"), g7, width = 12, height = 10)
+  ggsave(paste0(directory, "/plot_6_simulation_expression.pdf"), g9, width = 12, height = 12)
   
   ## GENERATE EXPERIMENT
   model <- model %>% 
@@ -45,12 +49,12 @@ complete_function <- function(model, directory) {
     model %>% 
     wrap_dyngen_dataset()
   
-  g8 <- dynplot::plot_dimred(traj)
-  g9 <- dynplot::plot_default(traj)
-  g <- patchwork::wrap_plots(g8, g9, nrow = 1)
-  g10 <- dynplot::plot_heatmap(traj, features_oi = 100)
+  g10 <- dynplot::plot_dimred(traj)
+  g11 <- dynplot::plot_default(traj)
+  g <- patchwork::wrap_plots(g10, g11, nrow = 1)
+  g12 <- dynplot::plot_heatmap(traj, features_oi = 100)
   ggsave(paste0(directory, "/plot_5_dataset_dimred.pdf"), g, width = 12, height = 6)
-  ggsave(paste0(directory, "/plot_6_heatmap.pdf"), g10, width = 12, height = 8)
+  ggsave(paste0(directory, "/plot_6_heatmap.pdf"), g12, width = 12, height = 8)
   
   ## SAVE R OBJECTS
   write_rds(traj, paste0(directory, "/out_dataset.rds"), compress = "xz")
@@ -58,10 +62,11 @@ complete_function <- function(model, directory) {
   
   # :scream:
   g <- patchwork::wrap_plots(
-    g1, g2, g3, g4, g5, g6, g7, g8, g9, g10,
+    g1, g2, g3, g4, g5, g6, g7, g8, g9, g10, g11, g12,
     byrow = FALSE,
-    ncol = 4
-  )
+    ncol = 1
+  ) +
+    patchwork::plot_annotation(tag_levels = "A")
   ggsave(paste0(directory, "/plot_all.pdf"), g, width = 40, height = 30)
   
   return()
