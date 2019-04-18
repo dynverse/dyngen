3ac548f7281042199a7127b1185527b88f1bd081
Robrecht Cannoodt <rcannood@gmail.com>
2019-04-18 13:13:19 +0200

    clean script

--- scripts_tmp/example_script.R
+++ scripts_tmp/example_script.R
@@ -1,16 +1,16 @@
 library(tidyverse)
 library(dyngen)
 
-set.seed(1)
+set.seed(2)
 model <- 
   initialise_model(
     num_cells = 1000,
     num_tfs = 60,
     num_targets = 200,
-    num_hks = 0,
+    num_hks = 500,
     distance_metric = "pearson",
-    backbone = backbone_bifurcating_cycle(),
-    tf_network_params = tf_network_random(min_tfs_per_module = 1),
+    backbone = backbone_bifurcating(),
+    tf_network_params = tf_network_random(min_tfs_per_module = 3),
     feature_network_params = feature_network_default(),
     kinetics_params = kinetics_custom(),
     gold_standard_params = gold_standard_default(time_per_edge = 2),
@@ -28,10 +28,11 @@ plot_backbone(model)
 plot_feature_network(model, tfs_only = TRUE)
 plot_feature_network(model)
 
-model <- model  %>% 
+model <- model %>% 
   generate_gold_standard()
 
 plot_gold_simulations(model, mapping = aes(comp_1, comp_2)) + scale_colour_brewer(palette = "Dark2")
+plot_gold_expression(model)
 
 model <- model %>% 
   generate_cells() %>% 
@@ -39,6 +40,7 @@ model <- model %>%
 
 plot_gold_mappings(model) + scale_colour_brewer(palette = "Dark2")
 plot_simulations(model)
+plot_simulation_expression(model)
 
 traj <- 
   model %>% 
@@ -52,33 +54,6 @@ dynplot::plot_heatmap(traj, features_oi = 100)
 
 
 
-edge_levels <- 
-  model$gold_standard$mod_changes %>% 
-  mutate(edge = paste0(from_, "->", to_)) %>% 
-  pull(edge)
-
-molecules <- model$feature_info %>% filter(is_tf) %>% gather(mol, val, x, y) %>% pull(val)
-df <- bind_cols(
-  model$gold_standard$meta,
-  as.data.frame(as.matrix(model$gold_standard$counts))[,molecules]
-) %>% 
-  gather(molecule, value, one_of(molecules)) %>% 
-  mutate(edge = factor(paste0(from_, "->", to_), levels = edge_levels)) %>% 
-  left_join(model$feature_info %>% select(x, y, module_id) %>% gather(type, molecule, x, y), by = "molecule") %>% 
-  group_by(module_id, t, simulation_i, burn, from, to, from_, to_, time, edge, type) %>% 
-  summarise(value = mean(value)) %>% 
-  ungroup()
-
-
-
-ggplot(df) +
-  geom_line(aes(t, value, colour = module_id, linetype = type), size = 2) +
-  facet_wrap(~edge) +
-  theme_bw()# +
-  # scale_color_manual(values = model$backbone$module_info %>% select(module_id, color) %>% deframe())
-  # scale_colour_brewer(palette = "Set3")
-
-model$gold_standard$mod_changes
 
 
 # TODO: also make plot for simulations
