eb568d3fbac1f8b81592fb0141145dbc813f33ff
Robrecht Cannoodt <rcannood@gmail.com>
2019-05-09 10:48:35 +0200

    update scripts

---
+++ scripts_tmp/datasets/trifurcating_1/model.R
@@ -0,0 +1,24 @@
+#!/usr/bin/Rscript
+
+library(dyngen)
+
+set.seed(4)
+model <- 
+  initialise_model(
+    num_cells = 1000,
+    num_tfs = 60,
+    num_targets = 400,
+    num_hks = 500,
+    distance_metric = "pearson",
+    backbone = backbone_trifurcating(),
+    tf_network_params = tf_network_random(min_tfs_per_module = 3, sample_num_regulators = function() 2),
+    feature_network_params = feature_network_default(),
+    kinetics_params = kinetics_custom(),
+    gold_standard_params = gold_standard_default(),
+    simulation_params = simulation_default(total_time = 10, num_simulations = 16),
+    experiment_params = experiment_snapshot(),
+    verbose = TRUE,
+    num_cores = 1,
+    download_cache_dir = "~/.cache/dyngen"
+  )
+dyngen:::complete_function(model, ".")
