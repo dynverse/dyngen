09536aaaf6ed3607d003e05476459b8f42b54614
Robrecht Cannoodt <rcannood@gmail.com>
2019-07-10 08:58:05 +0200

    add parameter to store grn into dynwrap dataset

--- R/9_dynwrap.R
+++ R/9_dynwrap.R
@@ -3,11 +3,12 @@
 #' The output of this object can be used with \pkg{dyno}.
 #' 
 #' @param model A dyngen output model for which the experiment has been emulated with [generate_experiment()].
+#' @param store_grn Whether or not to also store GRN information.
 #' 
 #' @export
 #' @importFrom dynwrap wrap_expression add_trajectory add_dimred
-wrap_dataset <- function(model) {
-  dynwrap::wrap_expression(
+wrap_dataset <- function(model, store_grn = FALSE) {
+  dataset <- wrap_expression(
     counts = model$experiment$xcounts,
     expression = as(log2(model$experiment$xcounts + 1), "dgCMatrix"),
     expression_projected = as(log2(model$experiment$wcounts + 1), "dgCMatrix"),
@@ -15,11 +16,11 @@ wrap_dataset <- function(model) {
     cell_info = model$experiment$cell_info %>% select(-from, -to, -time),
     feature_info = model$experiment$feature_info
   ) %>% 
-    dynwrap::add_trajectory(
+    add_trajectory(
       milestone_network = model$gold_standard$network,
       progressions = model$experiment$cell_info %>% select(cell_id, from, to, percentage = time)
     ) %>% 
-    dynwrap::add_dimred(
+    add_dimred(
       dimred = model$simulations$dimred[model$experiment$cell_info$step_ix, ] %>% 
         magrittr::set_rownames(model$experiment$cell_info$cell_id),
       dimred_projected = model$simulations$dimred_projected[model$experiment$cell_info$step_ix, ] %>% 
@@ -29,4 +30,14 @@ wrap_dataset <- function(model) {
         select(from, to, percentage = time),
       pair_with_velocity = TRUE
     )
+  
+  # add a few more values
+  if (store_grn) {
+    dataset$feature_network <- 
+      model$feature_network
+    dataset$feature_network_sc <- 
+      model$experiment$regulation
+  }
+  
+  dataset
 }
