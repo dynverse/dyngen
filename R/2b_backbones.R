cc5952df349835c14306ee6850fd374fc7ed2ad2
Robrecht Cannoodt <rcannood@gmail.com>
2019-07-10 16:41:02 +0200

    considering replacing backbones by lego implementations

--- R/2b_backbones.R
+++ R/2b_backbones.R
@@ -1,6 +1,14 @@
 #' @export
 #' @rdname backbone_models
 backbone_bifurcating <- function() {
+  # backbone <- backbone_lego(
+  #   backbone_lego_burn("A", num_modules = 2, type = "simple"),
+  #   backbone_lego_linear("A", "B", num_modules = 5),
+  #   backbone_lego_branching("B", c("C", "D"), num_modules = 4, type = "simple"),
+  #   backbone_lego_end("C", num_modules = 4),
+  #   backbone_lego_end("D", num_modules = 4)
+  # )
+  
   module_info <- tribble(
     ~module_id, ~a0, ~burn,
     "A1", 1, TRUE,
