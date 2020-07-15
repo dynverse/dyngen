12c44729a806c24de9ace340601d3b09f69de545
Robrecht Cannoodt <rcannood@gmail.com>
2020-07-15 09:07:33 +0200

    reexport GillespieSSA functions

---
+++ R/reexports.R
@@ -0,0 +1,11 @@
+#' @importFrom GillespieSSA2 ssa_etl
+#' @export
+GillespieSSA2::ssa_etl
+
+#' @importFrom GillespieSSA2 ssa_exact
+#' @export
+GillespieSSA2::ssa_exact
+
+#' @importFrom GillespieSSA2 ode_em
+#' @export
+GillespieSSA2::ode_em
