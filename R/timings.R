1e6fb3f2dd1c461e027842f686e220625e43f92d
Robrecht Cannoodt <rcannood@gmail.com>
2021-01-26 07:14:15 +0100

    drop do() as it is superseded, but summarise can handle this functionality now

--- R/timings.R
+++ R/timings.R
@@ -17,21 +17,13 @@
 get_timings <- function(model) {
   model$timings %>%
     group_by(.data$group) %>% 
-    do({
-      dat <- .
-      if (nrow(dat) > 1) {
-        tibble(
-          group = dat$group[[1]],
-          task = dat$task[-length(dat$task)],
-          time_elapsed = as.numeric(difftime(
-            dat$time[-1],
-            dat$time[-length(dat$time)],
-            units = "secs"
-          ))
-        )
-      } else {
-        NULL
-      }
-    }) %>% 
-    ungroup()
+    summarise(
+      task = .data$task[-length(.data$task)],
+      time_elapsed = as.numeric(difftime(
+        .data$time[-1],
+        .data$time[-length(.data$time)],
+        units = "secs"
+      )),
+      .groups = "drop"
+    ) 
 }
