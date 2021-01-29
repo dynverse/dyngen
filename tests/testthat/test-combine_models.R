40d6affa587d08e3a75d2c42c7d084fc5de38894
Robrecht Cannoodt <rcannood@gmail.com>
2021-01-29 09:52:02 +0100

    extend combine_models testing

--- tests/testthat/test-combine_models.R
+++ tests/testthat/test-combine_models.R
@@ -17,9 +17,9 @@ test_that("combining multiple models works", {
       ssa_algorithm = ssa_etl(tau = .5),
       experiment_params = simulation_type_wild_type(num_simulations = 1),
       census_interval = 2,
-      compute_cellwise_grn = FALSE, # todo: set this to true
+      compute_cellwise_grn = TRUE,
       compute_dimred = TRUE,
-      compute_rna_velocity = FALSE # todo: set this to true
+      compute_rna_velocity = TRUE
     ),
     verbose = FALSE
   ) %>% 
@@ -45,6 +45,68 @@ test_that("combining multiple models works", {
   expect_is(model_ab$simulations$meta, "data.frame")
   expect_true(any(grepl("left_", model_ab$simulations$meta$from)))
   expect_true(any(grepl("right_", model_ab$simulations$meta$from)))
+  
+  expect_is(model_ab$experiment$cell_info, "data.frame")
+  expect_true(any(grepl("left_", model_ab$experiment$cell_info$from)))
+  expect_true(any(grepl("right_", model_ab$experiment$cell_info$from)))
+  
+  expect_is(model_ab$simulations$dimred, "matrix")
+  expect_is(model_ab$experiment$cellwise_grn, "Matrix")
+  expect_is(model_ab$experiment$rna_velocity, "Matrix")
 })
 
 
+test_that("combining multiple models works", {
+  backbone <- backbone_linear()
+  
+  model_common <- initialise_model(
+    backbone = backbone,
+    num_cells = 10,
+    num_tfs = nrow(backbone$module_info),
+    num_targets = 5,
+    num_hks = 5,
+    gold_standard_params = gold_standard_default(
+      census_interval = 10,
+      tau = .5
+    ),
+    simulation_params = simulation_default(
+      ssa_algorithm = ssa_etl(tau = .5),
+      experiment_params = simulation_type_wild_type(num_simulations = 1),
+      census_interval = 2,
+      compute_cellwise_grn = FALSE,
+      compute_dimred = FALSE,
+      compute_rna_velocity = FALSE
+    ),
+    verbose = FALSE
+  ) %>% 
+    generate_tf_network() %>% 
+    generate_feature_network()
+  
+  model_a <- 
+    model_common %>% 
+    generate_kinetics() %>% 
+    generate_gold_standard() %>% 
+    generate_cells() %>% 
+    generate_experiment()
+  
+  model_b <- 
+    model_common %>% 
+    generate_kinetics() %>% 
+    generate_gold_standard() %>% 
+    generate_cells() %>% 
+    generate_experiment()
+  
+  model_ab <-
+    combine_models(list(left = model_a, right = model_b))
+  
+  expect_is(model_ab$simulations$meta, "data.frame")
+  expect_true(any(grepl("left_", model_ab$simulations$meta$from)))
+  expect_true(any(grepl("right_", model_ab$simulations$meta$from)))
+  
+  expect_is(model_ab$experiment$cell_info, "data.frame")
+  expect_true(any(grepl("left_", model_ab$experiment$cell_info$from)))
+  expect_true(any(grepl("right_", model_ab$experiment$cell_info$from)))
+  
+  expect_null(model_ab$experiment$cellwise_grn)
+  expect_null(model_ab$experiment$rna_velocity)
+})
