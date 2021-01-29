e26f38a8dc7c6a6cb0e8ca458da4a27551cfc879
Robrecht Cannoodt <rcannood@gmail.com>
2021-01-29 09:53:00 +0100

    add `as_SCE()` function

--- R/8_convert.R
+++ R/8_convert.R
@@ -31,7 +31,9 @@
 #'     total_time = 10,
 #'     census_interval = 1,
 #'     ssa_algorithm = ssa_etl(tau = 0.1),
-#'     experiment_params = simulation_type_wild_type(num_simulations = 1)
+#'     experiment_params = simulation_type_wild_type(num_simulations = 1),
+#'     compute_cellwise_grn = TRUE,
+#'     compute_rna_velocity = TRUE
 #'   )
 #' )
 #' }
@@ -165,7 +167,7 @@ as_anndata <- function(
   
   counts <- model$experiment$counts_mrna + model$experiment$counts_premrna
   
-  ad <- anndata::AnnData(
+  ad_args <- list(
     X = counts,
     layers = list(
       counts_spliced = model$experiment$counts_mrna,
@@ -190,25 +192,98 @@ as_anndata <- function(
   if (store_dimred) {
     dimred <- model$simulations$dimred[model$experiment$cell_info$step_ix, , drop = FALSE]
     rownames(dimred) <- model$experiment$cell_info$cell_id
-    ad$obsm$dimred <- dimred
+    ad_args$obsm$dimred <- dimred
     
-    ad$uns$traj_dimred_segments <- bind_cols(
+    ad_args$uns$traj_dimred_segments <- bind_cols(
       model$gold_standard$meta[!model$gold_standard$meta$burn, , drop = FALSE],
       as.data.frame(model$gold_standard$dimred[!model$gold_standard$meta$burn, , drop = FALSE])
     )
   }
   
   if (store_cellwise_grn) {
-    ad$uns$regulatory_network <- model$feature_network %>% 
+    ad_args$uns$regulatory_network <- model$feature_network %>% 
       select(regulator = .data$from, target = .data$to, .data$strength, .data$effect)
-    ad$obsm$regulatory_network_sc <- model$experiment$cellwise_grn
-    ad$uns$regulatory_network_regulators <- unique(model$feature_network$from)
-    ad$uns$regulatory_network_targets <- colnames(counts)
+    ad_args$obsm$regulatory_network_sc <- model$experiment$cellwise_grn
+    ad_args$uns$regulatory_network_regulators <- unique(model$feature_network$from)
+    ad_args$uns$regulatory_network_targets <- colnames(counts)
   }
   
   if (store_rna_velocity) {
-    ad$obsm$rna_velocity <- model$experiment$rna_velocity
+    ad_args$layers$rna_velocity <- model$experiment$rna_velocity
   }
   
-  ad
+  do.call(anndata::AnnData, ad_args)
+}
+
+#' @rdname convert
+#' @importFrom tibble column_to_rownames
+#' @export
+as_SCE <- function(
+  model,
+  store_dimred = !is.null(model$simulations$dimred),
+  store_cellwise_grn = !is.null(model$experiment$cellwise_grn),
+  store_rna_velocity = !is.null(model$experiment$rna_velocity)
+) {
+  requireNamespace("SingleCellExperiment")
+  requireNamespace("SummarizedExperiment")
+  
+  assert_that(
+    !is.null(model$experiment), 
+    msg = "model should be an object that was initialised with `initialise_model()`."
+  )
+  
+  counts <- model$experiment$counts_mrna + model$experiment$counts_premrna
+  
+  sce_args <- list(
+    assays = list(
+      counts = t(counts),
+      counts_spliced = t(model$experiment$counts_mrna),
+      counts_unspliced = t(model$experiment$counts_premrna),
+      counts_protein = t(model$experiment$counts_protein)
+    ),
+    colData = model$experiment$cell_info %>%
+      select(-.data$from, -.data$to, -.data$time) %>% 
+      as.data.frame() %>% 
+      column_to_rownames("cell_id"),
+    rowData = model$experiment$feature_info %>% 
+      as.data.frame() %>% 
+      column_to_rownames("feature_id"),
+    metadata = list(
+      traj_milestone_network = model$gold_standard$network,
+      traj_progressions = model$experiment$cell_info %>%
+        select(.data$cell_id, .data$from, .data$to, percentage = .data$time) %>% 
+        as.data.frame()
+    ),
+    reducedDims = list(),
+    altExps = list()
+  )
+  
+  if (store_dimred) {
+    dimred <- model$simulations$dimred[model$experiment$cell_info$step_ix, , drop = FALSE]
+    rownames(dimred) <- model$experiment$cell_info$cell_id
+    sce_args$reducedDims$MDS <- dimred
+    
+    sce_args$metadata$traj_dimred_segments <- bind_cols(
+      model$gold_standard$meta[!model$gold_standard$meta$burn, , drop = FALSE],
+      as.data.frame(model$gold_standard$dimred[!model$gold_standard$meta$burn, , drop = FALSE])
+    )
+  }
+  
+  if (store_cellwise_grn) {
+    sce_args$altExps$regulatory_network <- SummarizedExperiment::SummarizedExperiment(
+      assays = t(model$experiment$cellwise_grn),
+      rowData = model$feature_network %>% 
+        select(regulator = .data$from, target = .data$to, .data$strength, .data$effect),
+      metadata = list(
+        regulators = unique(model$feature_network$from),
+        targets = colnames(counts)
+      )
+    )
+  }
+  
+  if (store_rna_velocity) {
+    sce_args$assays$rna_velocity <- t(model$experiment$rna_velocity)
+  }
+  
+  do.call(SingleCellExperiment::SingleCellExperiment, sce_args)
 }
