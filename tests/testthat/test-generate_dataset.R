4233f1da6b746bdc67957f56f99c57e27dd52dc6
Robrecht Cannoodt <rcannood@gmail.com>
2021-02-22 12:22:31 +0100

    add some more checks for the dataset

--- tests/testthat/test-generate_dataset.R
+++ tests/testthat/test-generate_dataset.R
@@ -1,6 +1,7 @@
 context("generate_dataset")
 
 backbone <- backbone_linear()
+total_time <- simtime_from_backbone(backbone)
 
 init <- initialise_model(
   backbone = backbone,
@@ -14,8 +15,8 @@ init <- initialise_model(
   ),
   simulation_params = simulation_default(
     ssa_algorithm = ssa_etl(tau = .5),
-    experiment_params = simulation_type_wild_type(num_simulations = 1),
-    census_interval = 2,
+    experiment_params = simulation_type_wild_type(num_simulations = 2),
+    census_interval = 20,
     compute_cellwise_grn = TRUE,
     compute_dimred = TRUE,
     compute_rna_velocity = TRUE
@@ -25,14 +26,29 @@ init <- initialise_model(
 
 out <- generate_dataset(init, make_plots = TRUE)
 
-# skip if not rcannood because anndata is probably not installed
+test_that("output contains the desired objects", {
+  dataset <- out$dataset
+  expected_names <- c(
+    "cell_ids", "feature_ids", "counts", "counts_spliced", "counts_unspliced",
+    "counts_protein", "expression", "cell_info", "feature_info", "milestone_ids",
+    "milestone_network", "progressions", "milestone_percentages", "dimred", 
+    "dimred_segment_points", "dimred_segment_progressions", "regulatory_network",
+    "regulatory_network_sc", "regulators", "targets", "rna_velocity"
+  )
+  expect_true(all(expected_names %in% names(dataset)))
+  expect_true(all(sapply(expected_names, function(nam) !is.null(dataset[[nam]]))))
+  
+  # simple checks
+  expect_is(dataset$counts, "Matrix")
+  expect_equal(dataset$cell_ids, rownames(dataset$counts))
+  expect_equal(dataset$feature_ids, colnames(dataset$counts))
+})
+
+# skip on cran because some dependencies might be missing
 skip_on_cran()
-skip_if_not(Sys.info()[["user"]] == "rcannood")
 
 test_that("generating a dataset with linear backbone", {
   expect_is(out$plot, "ggplot")
-  
-  expect_true(dynwrap::is_wrapper_with_expression(out$dataset))
   
   # test converting to SCE
   sce <- as_sce(out$model)
@@ -45,6 +61,10 @@ test_that("generating a dataset with linear backbone", {
   # test converting to anndata
   ad <- as_anndata(out$model)
   expect_equal(dim(ad$X), dim(out$dataset$counts))
+  
+  # test converting to dyno
+  dataset <- as_dyno(out$model)
+  expect_equal(dim(dataset$counts), dim(out$dataset$counts))
 })
 
 
